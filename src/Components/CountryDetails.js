32f6809d1045688fb0c4b599e9fb7c984bb9852b
Thiago Evg <thiago.evg@gmail.com>
2021-04-18 16:13:02 -0300

    done

--- src/Components/CountryDetails.js
+++ src/Components/CountryDetails.js
@@ -16,42 +16,55 @@ function CountryDetails(props) {
     return country;
   }
 
-  return (
-    <div className="col-7">
-      <h1>{countryFound.name.common}</h1>
-      <table className="table">
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td style={{ width: '30%' }}>Capital</td>
-            <td>{countryFound.capital}</td>
-          </tr>
-          <tr>
-            <td>Area</td>
-            <td>
-              {countryFound.area} km
-              <sup>2</sup>
-            </td>
-          </tr>
-          <tr>
-            <td>Borders</td>
-            <td>
-              <ul style={{ listStyleType: 'none' }}>
-                {countryFound.borders.map((initials) => {
-                  const commonName = changeName(initials).name.common;
-                  return (
-                    <li key={initials}>
-                      <Link to={`/${initials}`}>{commonName}</Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  if (countryFound) {
+    return (
+      <div className="col-7">
+        <h1>{countryFound.name.common}</h1>
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{ width: '30%' }}>Capital</td>
+              <td>{countryFound.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {countryFound.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul style={{ listStyleType: 'none' }}>
+                  {countryFound.borders.map((initials) => {
+                    const commonName = changeName(initials).name.common;
+                    return (
+                      <li key={initials}>
+                        <Link to={`/${initials}`}>{commonName}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          display: 'block',
+          textAlign: 'center',
+        }}
+      >
+        <h4>Select a valid country</h4>
+      </div>
+    );
+  }
 }
 
 export default CountryDetails;
